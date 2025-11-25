631c76ad72825107903d3d865de808afa081b158
Juan Martin Bongiovanni <jmbongiovanni@mail.austral.edu.ar>
2025-11-25 17:20:31 -0300

    Un commit muy grande que no puedo ni explicar. Gracias =)

---
+++ R/ejercicio1.R
@@ -0,0 +1,15 @@
+#' Suma
+#'
+#' Sirve para sumar
+#'
+#' @param x Valor cualquiera
+#' @param y Otro valor cualquiera
+#'
+#' @examples
+#' suma(3,2)
+#'
+#' @returns Una suma
+#' @export
+suma <- function(x, y) {
+  x+y
+}
