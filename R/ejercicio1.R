9fa5d1d41373e0c226ae61781108823bb2e5ed75
Juan Martin Bongiovanni <jmbongiovanni@mail.austral.edu.ar>
2025-11-25 19:09:01 -0300

    haber si funca

--- R/ejercicio1.R
+++ R/ejercicio1.R
@@ -7,9 +7,15 @@
 #'
 #' @examples
 #' suma(3,2)
+#' suma("3",2)
 #'
 #' @returns Una suma
 #' @export
 suma <- function(x, y) {
-  x+y
+  if(!is.numeric(x)|!is.numeric(y)) {
+    cli::cli_inform("el x no equisnea y la y o inea/yenea")
+  } else {
+    x+y
+  }
+
 }
--- tests/testthat/test-suma.R
+++ tests/testthat/test-suma.R
@@ -1,3 +1,3 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("la suma funciona", {
+  expect_equal(object = suma(2, 2), expected = 4)
 })
