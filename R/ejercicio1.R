19a9fc039a27788f3ac807c6eeb400411585d8e8
Juan Martin Bongiovanni <jmbongiovanni@mail.austral.edu.ar>
2025-11-25 19:15:43 -0300

    otra prueba de test coverage

--- R/ejercicio1.R
+++ R/ejercicio1.R
@@ -8,12 +8,15 @@
 #' @examples
 #' suma(3,2)
 #' suma("3",2)
+#' suma(-3,2)
 #'
 #' @returns Una suma
 #' @export
 suma <- function(x, y) {
   if(!is.numeric(x)|!is.numeric(y)) {
     cli::cli_inform("el x no equisnea y la y o inea/yenea")
+  } if(x<0 | y<0) {
+    cli::cli_inform("el X es buto y la y lesbanana")
   } else {
     x+y
   }
